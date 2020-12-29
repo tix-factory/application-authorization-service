f682d04182ab54dde2477410d25176560e5f59ea
WebGL3D <OgreLandBob@gmail.com>
2020-12-29 08:06:03 -0800

    Init node service

---
+++ Node/application-authorization-service/implementation/authorizationHandler.js
@@ -0,0 +1,105 @@
+import { HttpRequest, HttpHandler, HttpRequestError, httpMethods } from "@tix-factory/http";
+const GuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+const CacheExpiryInMilliseconds = 60 * 1000;
+
+export default class extends HttpHandler {
+	constructor(logger) {
+		super();
+
+		this.logger = logger;
+		this.cache = {};
+	}
+
+	isAuthorized(apiKey, operation) {
+		if (operation.allowAnonymous) {
+			return Promise.resolve(true);
+		}
+
+		return new Promise(async (resolve, reject) => {
+			try {
+				const authorizedOperations = await this.getAuthorizedOperations(apiKey);
+				resolve(authorizedOperations.includes(operation.name.toLowerCase()));
+			} catch (e) {
+				this.logger.error(`Failed to load application authorizations for ApiKey.\n`, e);
+				resolve(false);
+			}
+		});
+	}
+
+	getAuthorizedOperations(apiKey) {
+		if (!apiKey || !GuidRegex.test(apiKey)) {
+			return Promise.resolve([]);
+		}
+
+		const currentTime = +new Date;
+		let cachedAuthorizations = this.cache[apiKey];
+		if (cachedAuthorizations) {
+			cachedAuthorizations.accessExpiry = currentTime + (CacheExpiryInMilliseconds * 2);
+			if (cachedAuthorizations.refreshExpiry < currentTime) {
+				cachedAuthorizations.refreshExpiry = currentTime + CacheExpiryInMilliseconds;
+				setTimeout(() => this.refreshAuthorizedOperations(apiKey), 0);
+			}
+
+			return Promise.resolve(cachedAuthorizations.operations);
+		}
+
+		return this.loadAuthorizationedOperations(apiKey);
+	}
+
+	refreshAuthorizedOperations(apiKey) {
+		this.loadAuthorizationedOperations(apiKey).then(() => {
+			// Loading the authorizations also cached them.
+		}).catch(err => {
+			this.logger.warn(`Failed to refresh application authorizations for ApiKey.\n`, err);
+		});
+	}
+
+	loadAuthorizationedOperations(apiKey) {
+		return new Promise(async (resolve, reject) => {
+			reject(new Error(`Authorization handler hasn't been implemented yet.`));
+			/*
+			try {
+				const httpRequest = new HttpRequest(httpMethods.post, this.getAuthorizedOperationsEndpoint);
+				httpRequest.addOrUpdateHeader("Tix-Factory-Api-Key", process.env.ApplicationApiKey);
+				httpRequest.addOrUpdateHeader("Content-Type", "application/json");
+				httpRequest.body = Buffer.from(JSON.stringify({
+					apiKey: apiKey
+				}));
+
+				const httpResponse = await this.httpClient.send(httpRequest);
+				if (httpResponse.statusCode === 200) {
+					const responseBody = JSON.parse(httpResponse.body.toString());
+					const authorizedOperations = responseBody.data.map(o => o.toLowerCase());
+					
+					this.cacheAuthorizations(apiKey, authorizedOperations);
+
+					resolve(authorizedOperations);
+					return;
+				} else {
+					reject(new HttpRequestError(httpRequest, httpResponse));
+				}
+			} catch (e) {
+				reject(e);
+			}
+			*/
+		});
+	}
+
+	cacheAuthorizations(apiKey, authorizedOperations) {
+		let currentTime = +new Date;
+		this.cache[apiKey] = {
+			accessExpiry: currentTime + (CacheExpiryInMilliseconds * 2),
+			refreshExpiry: currentTime + CacheExpiryInMilliseconds,
+			operations: authorizedOperations
+		};
+
+		setTimeout(() => {
+			const cachedAuthorizations = this.cache[apiKey];
+			let currentTime = +new Date;
+
+			if (cachedAuthorizations && cachedAuthorizations.accessExpiry < currentTime) {
+				delete this.cache[apiKey];
+			}
+		}, CacheExpiryInMilliseconds * 3);
+	}
+}
---
+++ Node/application-authorization-service/index.js
@@ -0,0 +1,76 @@
+import { dirname } from "path";
+import { fileURLToPath } from 'url';
+import { HttpServer } from "@tix-factory/http-service";
+import { MongoConnection } from "@tix-factory/mongodb";
+
+/*
+import ApplicationEntityFactory from "./entities/applicationEntityFactory.js";
+import ApplicationKeyEntityFactory from "./entities/applicationKeyEntityFactory.js";
+import OperationEntityFactory from "./entities/operationEntityFactory.js";
+import ApplicationOperationAuthorizationEntityFactory from "./entities/applicationOperationAuthorizationEntityFactory.js";
+*/
+
+import AuthorizationHandler from "./implementation/authorizationHandler.js";
+import KeyHasher from "./implementation/keyHasher.js";
+
+/*
+import DeleteApplicationSettingOperation from "./operations/DeleteApplicationSettingOperation.js";
+import GetApplicationSettingsOperation from "./operations/GetApplicationSettingsOperation.js";
+import SetApplicationSettingOperation from "./operations/SetApplicationSettingOperation.js";
+import SetApplicationSettingValueOperation from "./operations/SetApplicationSettingValueOperation.js";
+*/
+
+const workingDirectory = dirname(fileURLToPath(import.meta.url));
+
+const service = new HttpServer({
+    name: "TixFactory.ApplicationAuthorization.Service",
+    logName: "TFAAS2.TixFactory.ApplicationAuthorization.Service"
+});
+
+const init = () => {
+	console.log(`Starting ${service.options.name}...\n\tWorking directory: ${workingDirectory}\n\tNODE_ENV: ${process.env.NODE_ENV}\n\tPort: ${service.options.port}`);
+
+	return new Promise(async (resolve, reject) => {
+		try {
+			const mongoConnection = new MongoConnection(process.env.MongoConnectionString);
+			const applicationsCollection = await mongoConnection.getCollection("application-authorization-service", "applications");
+			const applicationKeysCollection = await mongoConnection.getCollection("application-authorization-service", "application-keys");
+			const operationsCollection = await mongoConnection.getCollection("application-authorization-service", "operations");
+			const applicationOperationAuthorizationsCollection = await mongoConnection.getCollection("application-authorization-service", "application-operation-authorizations");
+
+			const keyHasher = new KeyHasher();
+			const authorizationHandler = new AuthorizationHandler(service.logger);
+
+			/*
+			const applicationEntityFactory = new ApplicationEntityFactory(applicationsCollection);
+			const applicationKeyEntityFactory = new ApplicationKeyEntityFactory(applicationKeysCollection);
+			const operationEntityFactory = new OperationEntityFactory(operationsCollection);
+			const applicationOperationAuthorizationEntityFactory = new ApplicationOperationAuthorizationEntityFactory(applicationOperationAuthorizationsCollection);
+
+			await Promise.all([
+				applicationEntityFactory.setup(),
+				applicationKeyEntityFactory.setup(),
+				operationEntityFactory.setup(),
+				applicationOperationAuthorizationEntityFactory.setup()
+			]);
+			
+			service.operationRegistry.registerOperation(new DeleteApplicationSettingOperation(settingEntityFactory));
+			service.operationRegistry.registerOperation(new GetApplicationSettingsOperation(settingEntityFactory, applicationNameProvider));
+			service.operationRegistry.registerOperation(new SetApplicationSettingOperation(settingEntityFactory));
+			service.operationRegistry.registerOperation(new SetApplicationSettingValueOperation(settingEntityFactory, applicationNameProvider));
+			*/
+
+			resolve();
+		} catch (e) {
+			reject(e);
+		}
+	});
+};
+
+init().then(() => {
+	service.listen();
+}).catch(err => {
+	service.logger.error(err);
+	console.error(err);
+	process.exit(1);
+});
